edcc487556a1f5831a0a304379c8c4a20cb35ed3
“tramixx” <veneret@gmail.com>
2025-04-18 12:47:45 -0400

    fixing moving chart

--- WiseBooks/obj/Debug/net9.0/WiseBooks.AssemblyInfo.cs
+++ WiseBooks/obj/Debug/net9.0/WiseBooks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WiseBooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f36eb1252872b6b470a7ee1c8295644b3ecf2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb05a381e7a0f94cbab724aa2ff43d83b5e70f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WiseBooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WiseBooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
